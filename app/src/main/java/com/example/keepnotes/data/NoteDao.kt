4f5b82e4d483cf78f2ecf8889f6a8a3ef59864ff
yojan khuble <85030838+yojan15@users.noreply.github.com>
2024-01-31 11:32:45 +0530

    database created

---
+++ app/src/main/java/com/example/keepnotes/data/NoteDao.kt
@@ -0,0 +1,18 @@
+package com.example.keepnotes.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+@Dao
+interface NoteDao {
+    @Insert
+    fun insertNote(note: Note)
+    @Update
+    fun updateNote(note: Note)
+    @Delete
+    fun deleteNote(note: Note)
+    @Query("select * from note order by id ASC")
+    fun getAllNote():MutableList<Note>
+}
---
+++ app/src/main/java/com/example/keepnotes/data/NoteDatabase.kt
@@ -0,0 +1,25 @@
+package com.example.keepnotes.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1)
+abstract class NoteDatabase :RoomDatabase(){
+
+    abstract fun noteDao() : NoteDao
+    companion object {
+        private var INSTANCE : NoteDatabase? = null
+
+        fun getDatabase(context: Context):NoteDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                        NoteDatabase::class.java,"noteDB").build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
+}
