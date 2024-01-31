6bff81a16e3838feeabc143a5d7098f7e9854739
yojan khuble <85030838+yojan15@users.noreply.github.com>
2024-01-31 15:08:43 +0530

    on click fragment not working soo deleted that fragment

--- app/src/main/java/com/example/keepnotes/InsertFragment.kt
+++ app/src/main/java/com/example/keepnotes/InsertFragment.kt
@@ -1,7 +1,11 @@
 package com.example.keepnotes
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
+import com.example.keepnotes.data.Note
+import com.example.keepnotes.data.NoteDatabase
+import com.example.keepnotes.databinding.FragmentInsertBinding
 
 class InsertFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
